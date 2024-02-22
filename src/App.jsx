c74d30903d675d093f4dd174d628135eb4ba8870
imak02 <asbinkhanal222@gmail.com>
2024-02-22 19:02:58 +0545

    Simple static website

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <div className="text">
+          <h1>Hi I am Smri&apos;TEA&apos;</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
